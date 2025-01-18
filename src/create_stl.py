2c57519bc8b90b85f1adace459e3d29f1b579a96
Quentin Franklin <qfranklin906@gmail.com>
2025-01-18 04:24:48 -0500

    modified logo, will finish in new branch

--- src/create_stl.py
+++ src/create_stl.py
@@ -321,7 +321,7 @@ def qr_code():
         print("\n")
         
         logo_thickness = layer_height * 2
-        #generate_logo(vertices, faces, config.current_directory + "logo.png", 5, 5, logo_thickness, [12.5, 11.5])
+        generate_logo(vertices, faces, config.current_directory + "q_pyramid_logo.png", 5, 5, logo_thickness, [12.5, 12.5])
 
         #'''
 
